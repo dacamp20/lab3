0a495e310f017e6fbc7274591ec4908a102e514b
dacamp20 <156050102+dacamp20@users.noreply.github.com>
2024-01-31 15:33:55 -0800

    Updated test files

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 2, 4, 6 ,8 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 8, 6, 4, 2}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {2, 4, 6, 8 };
+    assertArrayEquals(new int[]{ 8, 6, 4, 2}, ArrayExamples.reversed(input1));
   }
 }
