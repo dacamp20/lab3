bd43cd53b366880960629dabc42ebe3c65a505ef
dacamp20 <156050102+dacamp20@users.noreply.github.com>
2024-01-31 16:11:46 -0800

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
+
+class MyStringChecker implements StringChecker{
+    public boolean checkString(String s){
+        return s.length() > 2;
+    }
+}
+
+public class ListTests {
+	@Test 
+	public void testFilter() {
+        MyStringChecker sc = new MyStringChecker();
+        List<String> input = new ArrayList<String>();
+        input.add("aaaa");
+
+        List<String> filterExpected = new ArrayList<String>();
+
+        assertArrayEquals(filterExpected.toArray(), ListExamples.filter(input, sc).toArray());
+	}
+}
